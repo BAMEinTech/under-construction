f31fbc09712b6bc6eb565807507bbe1b80b2c00d
Tanya Powell <hello@tanyapowell.co.uk>
2018-10-19 18:08:51 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./App.css";
+import { comingSoon } from "./content.json";
 
 class App extends Component {
+  _social() {}
+
   render() {
+    const content = comingSoon.content
+      .split(".")
+      .map((item, i) => <p key={i}>{item}</p>);
+
+    const social = Object.keys(comingSoon.footer).map((key, i) => {
+      const icon =
+        key === "twitter" || key === "instagram"
+          ? ["fab", key]
+          : key === "email"
+            ? "envelope"
+            : key;
+
+      const target = key === "email" ? null : "_blank";
+
+      return (
+        <a
+          key={i}
+          className="App-link"
+          href={comingSoon.footer[key]}
+          target={target}
+          rel="noopener noreferrer">
+          <FontAwesomeIcon key={i} icon={icon} size="2x" />
+        </a>
+      );
+    });
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>BAMEinTech</h1>
+          {content}
+          {social}
         </header>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+library.add(fab, faEnvelope, faTwitter);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
