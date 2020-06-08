8f8922155772d2fe0760bbc14c92b55e2dd7ca6d
Dionne Condor-Farrell <dcondor_farrell@yahoo.co.uk>
2020-06-08 15:20:00 +0100

    Revert "[WIP - Feat-#1] Adding sign up to mailing list"

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
